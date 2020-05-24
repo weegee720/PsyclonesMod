ec32426fd296ed6aff38dc9f8ba7004701cc7ffe
weegee <40016344+weegee720@users.noreply.github.com>
2020-05-24 20:20:33 +0300

    B16

---
+++ Psyclones.rte/Devices/Weapons/Wormhole.lua
@@ -0,0 +1,70 @@
+function Create(self)
+	self.fireTimer = Timer();
+	self.chargeCounter = 0;
+
+	self.maxCharge = 20;
+end
+
+function Update(self)
+	if self:IsActivated() then
+		if self.fireTimer:IsPastSimMS(200) then
+			self.chargeCounter = self.chargeCounter + 1
+			self.fireTimer:Reset();
+		end
+	else
+		if self.fireTimer:IsPastSimMS(200) and self.chargeCounter > 0 then
+			self.chargeCounter = self.chargeCounter - 1
+			self.fireTimer:Reset();
+		end
+	end
+	
+	if self.chargeCounter > 0 then
+		local actor = MovableMan:GetMOFromID(self.RootID);
+		if MovableMan:IsActor(actor) then
+			for i = 1 , MovableMan:GetMOIDCount() - 1 do
+				local mo = MovableMan:GetMOFromID(i);
+				if mo ~= nil then
+					if mo.RootID == actor.ID  then
+						if math.random(self.maxCharge) < self.chargeCounter then
+							local glownum = math.random(self.chargeCounter)
+							local pix = CreateMOPixel("Purple Glow "..tostring(glownum));
+							pix.Pos = mo.Pos
+							MovableMan:AddParticle(pix);
+						end
+					end
+				end
+			end
+		end
+	end
+	
+	if self.chargeCounter >= self.maxCharge then
+		if self.HFlipped == false then
+			self.reverseNum = 1;
+		else
+			self.reverseNum = -1;
+		end
+
+		local actor = MovableMan:GetMOFromID(self.RootID);
+
+		local damagePar = CreateAEmitter("Wormhole Shot");
+		damagePar.Pos = self.MuzzlePos + Vector(self.reverseNum,0):RadRotate(self.RotAngle);
+		damagePar.Vel = Vector(5*self.reverseNum,0):RadRotate(self.RotAngle);
+		damagePar:SetWhichMOToNotHit(MovableMan:GetMOFromID(self.RootID),-1);
+
+		if MovableMan:IsActor(actor) then
+			damagePar.Team = ToActor(actor).Team;
+			damagePar.IgnoresTeamHits = true;
+		end
+
+		MovableMan:AddParticle(damagePar);
+
+		local soundfx = CreateAEmitter("Dummy Laser Cannon Sound Fire");
+		soundfx.Pos = self.MuzzlePos;
+		MovableMan:AddParticle(soundfx);
+
+		self.chargeCounter = 0;
+	end
+end
+
+function Destroy(self)
+end
---
+++ Psyclones.rte/Devices/Weapons/WormholeShot.lua
@@ -0,0 +1,71 @@
+function Create(self)
+	self.lifeTimer = Timer();
+	self.emitTimer = Timer();
+
+	self.InitialVel = Vector(self.Vel.X, self.Vel.Y);
+end
+
+function Update(self)
+	if self.lifeTimer:IsPastSimMS(500) then
+		if self.emitTimer:IsPastSimMS(25) then
+			self.emitTimer:Reset();
+
+			local angle = math.pi * 2 * math.random()
+			
+			local pos = self.Pos + Vector(math.cos(angle) * 90, math.sin(angle) * 90);
+			if SceneMan:GetTerrMatter(pos.X, pos.Y) == 0 then
+				local damagePar = CreateMOPixel("Wormhole Emission Particle "..math.random(2));
+				
+				damagePar.Pos = pos;
+				damagePar.Vel = Vector(-(math.cos(angle) * 90), -(math.sin(angle) * 90));
+				damagePar.Team = self.Team;
+				damagePar.IgnoresTeamHits = true;
+				MovableMan:AddParticle(damagePar);
+			end
+		end
+	end
+
+	local pix = CreateMOPixel("Purple Glow ".. math.random(21));
+	pix.Pos = self.Pos - (self.Vel * 3)
+	MovableMan:AddParticle(pix);
+
+	if self.lifeTimer:IsPastSimMS(1000) then
+		local pix = CreateMOPixel("Purple Glow ".. math.random(15));
+		pix.Pos = self.Pos - (self.Vel * 4)
+		MovableMan:AddParticle(pix);
+	end
+
+	if self.lifeTimer:IsPastSimMS(1500) then
+		local pix = CreateMOPixel("Purple Glow ".. math.random(10));
+		pix.Pos = self.Pos - (self.Vel * 5)
+		MovableMan:AddParticle(pix);
+	end
+
+	if self.lifeTimer:IsPastSimMS(2000) then
+		local pix = CreateMOPixel("Purple Glow ".. math.random(5));
+		pix.Pos = self.Pos - (self.Vel * 6)
+		MovableMan:AddParticle(pix);
+	end
+
+	if self.lifeTimer:IsPastSimMS(2500) then
+		local pix = CreateMOPixel("Purple Glow ".. math.random(5));
+		pix.Pos = self.Pos - (self.Vel * 7)
+		MovableMan:AddParticle(pix);
+	end
+
+	if self.lifeTimer:IsPastSimMS(3000) then
+		local pix = CreateMOPixel("Purple Glow ".. math.random(5));
+		pix.Pos = self.Pos - (self.Vel * 8)
+		MovableMan:AddParticle(pix);
+	end
+	
+	if self.lifeTimer:IsPastSimMS(5500) then
+		for i = 1, 3 do
+			local pix = CreateMOSRotating("Terrain Eat Explosion");
+			pix.Pos = self.Pos + (self.Vel * (i * 4))
+			MovableMan:AddParticle(pix);
+			pix:GibThis();
+		end
+		self:GibThis();
+	end
+end
